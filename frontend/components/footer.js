7d9f35cce5b4a24c5d81a0b89b08d9badfddb787
Ricardo Luna <ricardodjesus96@gmail.com>
2025-10-31 23:28:30 -0600

    se crea footer css html y js para insertar footer dinamicamenteSE INSERTA FOOTER EN TODAS LAS PAGINAS (menos en carrito y confirmacion)

---
+++ frontend/components/footer.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("../components/footer.html")
+    .then(response => {
+      if (!response.ok) throw new Error("No se pudo cargar el footer");
+      return response.text();
+    })
+    .then(data => {
+      document.querySelector("footer").outerHTML = data;
+    })
+    .catch(error => console.error(error));
+});
