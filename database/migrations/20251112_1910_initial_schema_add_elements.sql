58f66251752e0e47a184704bcf0ffaead786c575
Brad Robles <bradmrh@gmail.com>
2025-11-12 19:57:52 -0600

    Actializo los documentos de bases de datos

---
+++ database/migrations/20251112_1910_initial_schema_add_elements.sql
@@ -0,0 +1,18 @@
+-- Script para agregar 
+insert into product (id_product, product_name, descripcion, image, formato_venta, cantidad_stock, tamano, precio, categoria, descuento) values
+();
+
+insert into usuario (id_usuario, nombre, apellidos, email, telefono, fecha_nacimiento, contrasena) values
+();
+
+insert into pedido (id_pedido, id_usuario, direccion, total_venta, numero_rastreador, fecha_crecion) values
+();
+
+insert into pedido_detalle(id_pedido_detalle, id_pedido, id_producto, cantidad, precio) values
+();
+
+insert into formato_producto(id_formato_producto, formato) values
+();
+
+
+
